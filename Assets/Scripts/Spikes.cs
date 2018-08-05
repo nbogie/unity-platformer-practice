1d846248f2dfac96e73bd58b621a137edc9aae0e
Neill <neillbogie@googlemail.com>
2018-08-05 21:44:00 +0100

    Added Spikes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+    AudioSource src;
+	// Use this for initialization
+	void Start () {
+        src = GetComponent<AudioSource>();
+	}
+	
+	
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player)
+        {
+            player.TakeDamage(10000f);
+            src.Play();
+        }
+    }
+}
