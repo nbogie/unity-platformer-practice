ce96fb363fe4037cf66c9a080d84be0df0f5c39d
Neill <neillbogie@googlemail.com>
2018-08-06 14:51:36 +0100

    Added animated falling spikes

---
+++ Assets/Scripts/FallingSpikes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour {
+    private bool triggered;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player)
+        {
+            if (!triggered)
+            {
+                player.TakeDamage(10000);
+                triggered = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player)
+        {
+            if (triggered)
+            {
+                triggered = false;
+            }
+        }
+
+    }
+}
