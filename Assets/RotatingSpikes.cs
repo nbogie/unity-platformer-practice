efe6f1d87139174eeb2c61d8a5267a7eddd50c15
Neill <neillbogie@googlemail.com>
2018-08-06 15:21:02 +0100

    Added Rotating Spikes

---
+++ Assets/RotatingSpikes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingSpikes : MonoBehaviour {
+    [SerializeField]
+    private float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        transform.Rotate(Vector3.back, rotSpeed * Time.fixedDeltaTime);	
+	}
+}
